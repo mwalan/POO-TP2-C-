569028c9e6f67838e82655fc5bf035e8de59c2ab
mwalan <116664796+mwalan@users.noreply.github.com>
2024-07-08 00:53:24 -0300

    Update main.cpp

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <filesystem>
+#include <exception>
+
+#include "PPGI.hpp"
+#include "Readers.hpp"
+#include "Reports.hpp"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    try {
+        string diretorio = Readers::readDiretorio(argc, argv);
+        vector<int> dataRecredenciamento = Readers::readAnoRecredenciamento();
+
+        PPGI ufes;
+        Readers::readPosGraduacao(diretorio, dataRecredenciamento, ufes);
+
+        Reports::writeRelatorios(diretorio, dataRecredenciamento, ufes);
+
+    } catch (const exception& e) {
+        
+        Reports::createRelatorios("");
+        cerr << "Erro: " << e.what() << endl;
+        return 1; // Indica erro
+    }
+
+    return 0; // Indica sucesso
+}
